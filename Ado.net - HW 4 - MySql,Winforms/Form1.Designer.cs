93460367b6841e8c7221fd7c9fe5624e260d262f
Bl0ck <zail@dublin.com>
2018-07-10 23:46:27 +0300

    Add project files.

---
+++ Ado.net - HW 4 - MySql,Winforms/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Ado.net___HW_4___MySql_Winforms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.comboBoxTables = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBoxSeller = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBoxBuyer = new System.Windows.Forms.ComboBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBoxProducts = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 287);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(552, 295);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// comboBoxTables
+			// 
+			this.comboBoxTables.FormattingEnabled = true;
+			this.comboBoxTables.Location = new System.Drawing.Point(66, 257);
+			this.comboBoxTables.Name = "comboBoxTables";
+			this.comboBoxTables.Size = new System.Drawing.Size(121, 24);
+			this.comboBoxTables.TabIndex = 1;
+			this.comboBoxTables.SelectedIndexChanged += new System.EventHandler(this.comboBoxTables_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 257);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(48, 17);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Table:";
+			// 
+			// comboBoxSeller
+			// 
+			this.comboBoxSeller.FormattingEnabled = true;
+			this.comboBoxSeller.Location = new System.Drawing.Point(63, 13);
+			this.comboBoxSeller.Name = "comboBoxSeller";
+			this.comboBoxSeller.Size = new System.Drawing.Size(121, 24);
+			this.comboBoxSeller.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 16);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(48, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Seller:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(261, 16);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(49, 17);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Buyer:";
+			// 
+			// comboBoxBuyer
+			// 
+			this.comboBoxBuyer.FormattingEnabled = true;
+			this.comboBoxBuyer.Location = new System.Drawing.Point(315, 13);
+			this.comboBoxBuyer.Name = "comboBoxBuyer";
+			this.comboBoxBuyer.Size = new System.Drawing.Size(121, 24);
+			this.comboBoxBuyer.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(15, 76);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(61, 17);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Product:";
+			// 
+			// comboBoxProducts
+			// 
+			this.comboBoxProducts.FormattingEnabled = true;
+			this.comboBoxProducts.Location = new System.Drawing.Point(92, 76);
+			this.comboBoxProducts.Name = "comboBoxProducts";
+			this.comboBoxProducts.Size = new System.Drawing.Size(445, 24);
+			this.comboBoxProducts.TabIndex = 8;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(108, 156);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(328, 42);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Order";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(576, 594);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBoxProducts);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBoxBuyer);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBoxSeller);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBoxTables);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.ComboBox comboBoxTables;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBoxSeller;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBoxBuyer;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox comboBoxProducts;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Ado.net - HW 4 - MySql,Winforms/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Ado.net___HW_4___MySql_Winforms
+{
+	public static class MyTransaction
+	{
+		public static void EnlistTransaction(this MySqlDataAdapter dataAdapter, MySqlTransaction transaction)
+		{
+			MySqlCommandBuilder mySqlCommandBuilder = new MySqlCommandBuilder(dataAdapter);
+			dataAdapter.UpdateCommand.Transaction = transaction;
+			dataAdapter.InsertCommand.Transaction = transaction;
+			dataAdapter.DeleteCommand.Transaction = transaction;
+		}
+	}
+	public partial class Form1 : Form
+	{
+		MySqlConnection sqlConnection;
+		DataSet dataSet;
+		MySqlDataAdapter dataAdapter;
+		public Form1()
+		{
+			InitializeComponent();
+
+			MySqlConnectionStringBuilder sqlConnectionStringBuilder = new MySqlConnectionStringBuilder();
+			sqlConnectionStringBuilder.Server = "91.217.9.155";
+			sqlConnectionStringBuilder.Database = "krivbass_dz";
+			sqlConnectionStringBuilder.UserID = "krivbass_dz";
+			sqlConnectionStringBuilder.Password = "itstep";
+			sqlConnectionStringBuilder.SslMode = MySqlSslMode.None;
+
+
+			sqlConnection = new MySqlConnection(sqlConnectionStringBuilder.ConnectionString);
+
+			Load += Form1_Load;
+		}
+		/*
+Разработать приложение для магазина холодильников.
+Хранить товар, продавцов и покупателей.
+Когда покупатель приобретает товар, оформляется чек. 
+Чек содержит дату, ФИО покупателя, ФИО продавца и купленные позиции. 
+Процесс покупки хлодильника осущиствить через механизм транзакцию 
+(Вставка строки в таблицу для хранения чеков и Снятие с остатков у покупателя и изменение количества купленных товаров у покупателя)*/
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			string[] createCommands = { @"CREATE TABLE IF NOT EXISTS Buyers (
+								id INT not null AUTO_INCREMENT,
+								name varchar(100) not null,
+								boughtCount int null default 0,
+								PRIMARY KEY(id))",
+				@"CREATE TABLE IF NOT EXISTS Sellers (
+								id INT not null  AUTO_INCREMENT,
+								name varchar(100) not null,
+								soldCount int null default 0,
+								PRIMARY KEY(id))",
+				@"CREATE TABLE IF NOT EXISTS Goods (
+								id INT not null  AUTO_INCREMENT,
+								name varchar(100) not null,
+								price INT not null,
+								count INT not null,
+								PRIMARY KEY(id))",
+				@"CREATE TABLE IF NOT EXISTS Orders (
+								id INT not null  AUTO_INCREMENT,
+								date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+								buyerName varchar(100) not null,
+								sellerName varchar(100) not null,
+								good_id INT not null,
+								count INT not null,
+								amount INT not null,
+								PRIMARY KEY(id))" };
+			try
+			{
+				sqlConnection.Open();
+
+				foreach (var item in createCommands)
+				{
+					try
+					{
+						new MySqlCommand(item, sqlConnection).ExecuteNonQuery();
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show(ex.Message, "1");
+					}
+				}
+				try
+				{
+					new MySqlCommand(@"insert into Buyers VALUES (1, 'Kostya', 0), (2, 'Olena', 0), (3, 'Frederich', 0)",
+						sqlConnection).ExecuteNonQuery();
+					new MySqlCommand(@"insert into Sellers VALUES (1, 'Vasiliy', 0), (2, 'Konstantin', 0), (3, 'Marina', 0)",
+						sqlConnection).ExecuteNonQuery();
+					new MySqlCommand(@"insert into Goods VALUES (1, 'Nord', 5000, 400), (2, 'LG', 9000, 250), (3, 'Apsheron', 800, 10), (4, 'Vesna', 600, 50)",
+						sqlConnection).ExecuteNonQuery();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message, "2");
+				}
+
+
+				dataSet = new DataSet();
+				dataAdapter = new MySqlDataAdapter("select * from Buyers", sqlConnection);
+				MySqlCommandBuilder mySqlCommandBuilder = new MySqlCommandBuilder(dataAdapter);
+				dataAdapter.Fill(dataSet, "Buyers");
+				dataGridView1.DataSource = dataSet.Tables[0];
+				dataAdapter.SelectCommand = new MySqlCommand("select * from Sellers", sqlConnection);
+				dataAdapter.Fill(dataSet, "Sellers");
+				dataAdapter.SelectCommand = new MySqlCommand("select * from Goods", sqlConnection);
+				dataAdapter.Fill(dataSet, "Goods");
+				dataAdapter.SelectCommand = new MySqlCommand("select * from Orders", sqlConnection);
+				dataAdapter.Fill(dataSet, "Orders");
+
+				foreach (DataTable table in dataSet.Tables)
+				{
+					comboBoxTables.Items.Add(table.TableName);
+				}
+				comboBoxTables.SelectedIndex = 0;
+
+				foreach (DataRow item in dataSet.Tables["Buyers"].Rows)
+				{
+					comboBoxBuyer.Items.Add(item.ItemArray[1].ToString());
+				}
+				comboBoxBuyer.SelectedIndex = 0;
+
+				foreach (DataRow item in dataSet.Tables["Sellers"].Rows)
+				{
+					comboBoxSeller.Items.Add(item.ItemArray[1].ToString());
+				}
+				comboBoxSeller.SelectedIndex = 0;
+
+				foreach (DataRow item in dataSet.Tables["Goods"].Rows)
+				{
+					comboBoxProducts.Items.Add(item.ItemArray[1].ToString() + " - ₴" +
+						item.ItemArray[2].ToString());
+				}
+				comboBoxProducts.SelectedIndex = 0;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "3");
+			}
+			finally
+			{
+				sqlConnection?.Clone();
+			}
+		}
+
+		private void comboBoxTables_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			dataGridView1.DataSource = dataSet.Tables[comboBoxTables.SelectedIndex];
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (MySqlTransaction mySqlTransaction = sqlConnection.BeginTransaction())
+				{
+					dataAdapter.EnlistTransaction(mySqlTransaction);
+
+					int count = 1;
+					DataRow dr = dataSet.Tables["Orders"].NewRow();
+					dr[0] = dataSet.Tables["Orders"].Rows.Count;
+					dr[1] = DateTime.Now;
+					dr[2] = dataSet.Tables["Buyers"].Rows[comboBoxBuyer.SelectedIndex][1].ToString();
+					dr[3] = dataSet.Tables["Sellers"].Rows[comboBoxSeller.SelectedIndex][1].ToString();
+					dr[4] = comboBoxProducts.SelectedIndex;
+					dr[5] = count;
+					dr[6] = dataSet.Tables["Goods"].Rows[comboBoxProducts.SelectedIndex][2];
+
+					dr = dataSet.Tables["Buyers"].Rows[comboBoxBuyer.SelectedIndex];
+					dr[2] = (int)dr[2] + 1;
+					dr = dataSet.Tables["Sellers"].Rows[comboBoxSeller.SelectedIndex];
+					dr[2] = (int)dr[2] + 1;
+					dr = dataSet.Tables["Goods"].Rows[comboBoxProducts.SelectedIndex];
+					dr[2] = (int)dr[3] - 1;
+
+					dataAdapter.Update(dataSet);
+
+					mySqlTransaction.Commit();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}
